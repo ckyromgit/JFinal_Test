ab09b1555bc46df50643ca8ead84896909f6f937
keyou.chen <keyou.chen@payegis.com>
2017-03-24 17:55:22 +0800

    Initial commit

---
+++ jfinal-web/src/main/java/HelloController.java
@@ -0,0 +1,11 @@
+/**
+ * Created by keyou.chen on 2017/3/24.
+ */
+
+import com.jfinal.core.Controller;
+
+public class HelloController extends Controller {
+    public void index(){
+        renderText("hello JFinal!");
+    }
+}
